2d69d6f7a4b8c2242fcb5e5cc299eaa313261676
KouichiSano <snk.kouichi@gmail.com>
2018-07-07 08:54:18 +0000

    model change

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -2,4 +2,7 @@ class Chapter < ApplicationRecord
 	belongs_to :story
 	has_many :comments,:dependent => :destroy
 	validates :subtitle,    length: { maximum: 50 }
+
+	scope :起承転結, -> { where(part: %w(起 承 転 結)).order(id: :asc) }
+
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,7 +1,10 @@
 class Story < ApplicationRecord
 	belongs_to :user
-	has_many :chapters,:dependent => :destroy
+	has_many :chapters, dependent: :destroy
+	delegate :builds, to: :chapters, prefix: :chapters
+	has_many :起承転結, -> { 起承転結 }, class_name: "Chapter"
+
+
 	validates :title,    length: { in: 1..200 }
 	validates :genre,    length: { in: 1..50 }
-	
 end
